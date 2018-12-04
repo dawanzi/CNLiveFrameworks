a4f43b3092ded3582984305d00cf9796142abd89
张旭 <zhangxu@cnlive.com>
2018-12-04 10:45:47 +0800

    0.1.5

---
+++ 基础层/CNLiveBaseKit/CNLiveBaseKit.framework/Headers/NSString+CNLiveExtension.h
@@ -0,0 +1,15 @@
+//
+//  NSString+CNLiveExtension.h
+//  CNLiveCommonCategory
+//
+//  Created by 张旭 on 2018/9/26.
+//  Copyright © 2018年 cnlive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (CNLiveExtension)
+
+- (BOOL)isEmptyString;
+
+@end
