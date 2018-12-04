8576714bc2dafbb0c5b3546f7220e539ac1bc3c6
张旭 <zhangxu@cnlive.com>
2018-12-04 13:22:22 +0800

    no message

---
+++ CNLiveBaseKit.framework/Headers/CNLiveBaseKit-umbrella.h
@@ -0,0 +1,33 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "BaseModel.h"
+#import "CNLiveBaseKit.h"
+#import "NSString+CNLiveExtension.h"
+#import "NSString+Common.h"
+#import "CNSetUserDefaultTool.h"
+#import "UIFont+CNLiveExtension.h"
+#import "UILabel+Common.h"
+#import "UIView+CNAlertView.h"
+#import "UIView+Extension.h"
+#import "CNLiveDefinesHeader.h"
+#import "ColorHeaders.h"
+#import "ColorMacro.h"
+#import "PathHeaders.h"
+#import "PathUtility.h"
+#import "UIColor+MLPFlatColors.h"
+#import "GTMBase64.h"
+#import "GTMDefines.h"
+
+FOUNDATION_EXPORT double CNLiveBaseKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char CNLiveBaseKitVersionString[];
+
